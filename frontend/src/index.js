5bc079a0be38003a5f8db948998bfd68e6b6a8f0
Avani Dhoot <adhoot@DSRM.local>
2018-07-17 16:57:01 +0530

    first commit

---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './modules/common/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
---
+++ frontend/src/modules/common/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+    render() {
+        return (
+
+
+            <p >
+                Header
+            </p>
+
+        );
+    }
+}
+
+export default Header;
