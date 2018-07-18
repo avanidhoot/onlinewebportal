f2b6aa91456fbaff761ef0d95791dd07b068dff4
Avani Dhoot <adhoot@DSRM.local>
2018-07-18 11:13:36 +0530

    few changes

--- frontend/src/modules/common/App.js
+++ frontend/src/modules/common/App.js
@@ -5,10 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-
           <h1 className="App-title">Welcome to Web Portal Online Deals</h1>
-        </header>
         <div className="App-intro">
          <Login/>
         </div>
