36926da56b79e9d5e2b34f6974b4377dfe147c71
Ganesh Khedkar <gkhedkar@DSRM.local>
2018-07-18 11:59:03 +0530

    sematic UI css adeed

--- frontend/src/modules/common/Header.js
+++ frontend/src/modules/common/Header.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
-
+import { Menu } from 'semantic-ui-react'
 
 class Header extends Component {
+      state = { activeItem: 'home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
     render() {
+        const { activeItem } = this.state
         return (
-
-
-            <p >
-                Header
-            </p>
-
+            
+            <Menu inverted>
+                <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} />
+                <Menu.Item
+                name='messages'
+                active={activeItem === 'messages'}
+                onClick={this.handleItemClick}
+                />
+                <Menu.Item
+                name='friends'
+                active={activeItem === 'friends'}
+                onClick={this.handleItemClick}
+                />
+            </Menu>
         );
     }
 }
 
 export default Header;
+
