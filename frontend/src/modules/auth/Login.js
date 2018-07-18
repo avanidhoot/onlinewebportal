c653036f3deca4db436dada2d41b67eaabe6d13b
Ganesh Khedkar <gkhedkar@DSRM.local>
2018-07-18 12:01:10 +0530

    new changes

--- frontend/src/modules/auth/Login.js
+++ frontend/src/modules/auth/Login.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
-
+import { Form, Input,Button } from 'semantic-ui-react'
 
 class Login extends Component {
     render() {
         return (
+            <div>
+                <Form>
+                    <Form.Field required>
+                        <label>Email</label>
+                        <Input placeholder='Email' />
+                    </Form.Field>
+                    <Form.Field required>
+                        <label>Password</label>
+                        <Input type='password' placeholder='Password' />
+                    </Form.Field>
+                    <Button type='submit'>Submit</Button>
+                </Form>
+            </div>
                 <p >
                     Login
                 </p>
--- frontend/src/modules/common/App.js
+++ frontend/src/modules/common/App.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
+import Login from "../auth/Login";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <h1 className="App-title">Welcome to Web Portal Online Deals</h1>
+        <div className="App-intro">
+         <Login/>
+        </div>
       </div>
     );
   }
